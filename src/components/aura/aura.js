083647bc0327e1101f35f178ccb5af4aa9dc2672
Ankoor Patel <ankoor.patel@bbc.co.uk>
2016-12-26 19:43:44 +0000

    test a component sub generator

---
+++ src/components/aura/aura.js
@@ -0,0 +1,22 @@
+import React, { Component, PropTypes } from 'react'
+
+import './aura.scss'
+
+
+export default class aura extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  static propTypes = {
+
+  };
+
+  render () {
+    return (
+      <div className='aura'>
+
+      </div>
+    )
+  }
+}
